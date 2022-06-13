51d5badb619712ac6ec324a96f1d43b9fa396d69
ji hyeon <jihyeon.acc@gmail.com>
2022-06-13 17:04:39 +0900

    Modify: change test class configuration due to spring security

--- src/test/java/com/personalproject/homepage/controller/CategoryRestControllerTest.java
+++ src/test/java/com/personalproject/homepage/controller/CategoryRestControllerTest.java
@@ -14,18 +14,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.personalproject.homepage.dto.CategoryDto;
-import com.personalproject.homepage.error.ApiException;
-import com.personalproject.homepage.error.ErrorMessage;
-import com.personalproject.homepage.service.CategoryService;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -34,11 +29,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.personalproject.homepage.config.CustomUnitTestSecurityConfig;
+import com.personalproject.homepage.dto.CategoryDto;
+import com.personalproject.homepage.error.ApiException;
+import com.personalproject.homepage.error.ErrorMessage;
+import com.personalproject.homepage.service.CategoryService;
+
 @WebMvcTest(CategoryRestController.class)
+@CustomUnitTestSecurityConfig
+@WithMockUser(roles = "ADMIN") // 모든 접근 권한을 가진 MockUser를 설정 한다.
 @ActiveProfiles("test")
 public class CategoryRestControllerTest {
 
@@ -355,6 +360,7 @@ public class CategoryRestControllerTest {
             );
 
             // then
+            result.andDo(print());
             verify(categoryService).updateCategory(any(CategoryDto.class), any(CategoryDto.class));
             result.andExpect(matchAll(
                 status().isOk(),
